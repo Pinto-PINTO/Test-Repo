175102db9e17981aa0769585aca8e0b563721103
Menuka Pinto <instafeed9999@gmail.com>
2021-07-12 19:29:08 +0530

    new feature implemented on new-feature branch

--- trial3.py
+++ trial3.py
@@ -1,3 +1,8 @@
 print('trial3: Information after trial')
 print('trial3: Information here')
 print('trial3: Information again')
+print('trial3: Information again')
+print('trial3: Information again')
+print('trial3: Information again')
+print('trial3: Information again')
+print('trial3: Information again')
